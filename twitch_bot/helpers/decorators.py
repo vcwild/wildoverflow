16fa4fe05a578d6c48d491f17dca9a098667db29
Victor <vcwildm@gmail.com>
2021-07-25 02:57:44 -0300

    create is mod decorator

--- twitch_bot/helpers/decorators.py
+++ twitch_bot/helpers/decorators.py
@@ -1,7 +1,18 @@
+from functools import wraps
+
+
 def run_once(f):
-	def wrapper(*args, **kwargs):
-		if not wrapper.has_run:
-			wrapper.has_run = True
-			return f(*args, **kwargs)
-	wrapper.has_run = False
-	return wrapper
+    def wrapper(*args, **kwargs):
+        if not wrapper.has_run:
+            wrapper.has_run = True
+            return f(*args, **kwargs)
+    wrapper.has_run = False
+    return wrapper
+
+
+def is_mod(f):
+    @wraps(f)
+    async def wrapper(self, ctx):
+        if ctx.author.is_mod:
+            await f(self, ctx)
+    return
