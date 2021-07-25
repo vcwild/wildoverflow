5c65db4453bf860d97607dcb9ffc21a7c351f0c3
Victor <vcwildm@gmail.com>
2021-07-25 03:18:08 -0300

    update root dir location

--- twitch_bot/helpers/files.py
+++ twitch_bot/helpers/files.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 
 def read_yaml(file: str):
-    ROOT_DIR = Path(__file__).parent.parent.parent
+    ROOT_DIR = Path(__file__).parent.parent
 
-    directory = str(ROOT_DIR) + "/data/"
+    directory = str(ROOT_DIR) + "/database/"
 
     with open(directory + file, 'r') as file:
         stream = file.read()
