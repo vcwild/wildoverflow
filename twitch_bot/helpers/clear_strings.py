56f8db0b9aaec14e6a7c87efd922d518cb086963
Victor <vcwildm@gmail.com>
2021-08-28 01:50:56 -0300

    fix : refactor cache

--- twitch_bot/helpers/clear_strings.py
+++ twitch_bot/helpers/clear_strings.py
@@ -1,13 +1,18 @@
 def _remove_trailing_comma(string: str):
-	if string[-1] == ',':
-		return string.rstrip(string[-1])
-	return string
+    if string[-1] == ",":
+        return string.rstrip(string[-1])
+    return string
+
 
 def parse_string(string: str):
-	string = _remove_trailing_comma(string)
-	return (str(string)
-				.replace('\t', '')
-				.replace('\n', '')
-				.replace('\\', '')
-				.strip()
-				.split(','))
+    string = _remove_trailing_comma(string)
+    return (
+        str(string)
+        .replace("\t", "")
+        .replace("\n", "")
+        .replace("\\", "")
+        .replace("[", "")
+        .replace("]", "")
+        .strip()
+        .split(",")
+    )
