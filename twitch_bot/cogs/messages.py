5302aec9fa7c65b5516d12a433f7b4c872a80746
Victor <vcwildm@gmail.com>
2021-08-29 00:52:02 -0300

    fix: remove messages module

--- twitch_bot/cogs/messages.py
+++
@@ -1,9 +0,0 @@
-from twitch_bot.cogs.data_loader import DataParser
-
-
-class Messages:
-
-    def __init__(self):
-        self.greetings = DataParser.greetings
-        self.initial_greetings = DataParser.initial_greetings
-        self.commands = DataParser.commands
