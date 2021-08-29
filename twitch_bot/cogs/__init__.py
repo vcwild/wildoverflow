c085a27ecc4bc9c18584380e7f1240577c5feaab
Victor <vcwildm@gmail.com>
2021-08-29 00:52:53 -0300

    fix: update bot handling methods

--- twitch_bot/cogs/__init__.py
+++ twitch_bot/cogs/__init__.py
@@ -1,1 +1,5 @@
-from .command_cog import TwitchBot
+from twitch_bot.cogs.twitch import TwitchBot
+from twitch_bot.cogs.event import EventCog
+from twitch_bot.cogs.message import MessageCog
+from twitch_bot.cogs.management import ManagementCog
+from twitch_bot.cogs.play import PlayCog
