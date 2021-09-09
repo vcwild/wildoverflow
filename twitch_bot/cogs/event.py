91380748e0f8585aaf48f7ff5d087c2e525f4066
Victor Wildner <vcwildm@gmail.com>
2021-09-09 11:00:50 -0300

    feat: add typing to event module

--- twitch_bot/cogs/event.py
+++ twitch_bot/cogs/event.py
@@ -2,5 +2,5 @@ from twitch_bot.cogs.plugin import Plugin
 
 
 class EventCog(Plugin):
-    async def event_ready(self):
+    async def event_ready(self) -> None:
         self.logger.warning(f"{self.__class__.__name__} is plugged in!")
