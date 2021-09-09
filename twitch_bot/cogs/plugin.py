b13abae7cbfa213d3425eacea5c7569f9b1caf27
Victor Wildner <vcwildm@gmail.com>
2021-09-09 11:01:24 -0300

    feat: add typing to plugin module

--- twitch_bot/cogs/plugin.py
+++ twitch_bot/cogs/plugin.py
@@ -1,9 +1,10 @@
 import logging
+from twitch_bot.cogs.twitch import TwitchBot
 
 
 class Plugin:
     """Class for all bot plugins"""
 
-    def __init__(self, bot):
+    def __init__(self, bot: TwitchBot):
         self.bot = bot
         self.logger = logging.getLogger(__name__)
