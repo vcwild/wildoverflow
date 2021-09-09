fa4d9e5ed2756929e089c6462c9f756c1b091d8d
Victor Wildner <vcwildm@gmail.com>
2021-09-09 11:01:12 -0300

    feat: add typing to play module

--- twitch_bot/cogs/play.py
+++ twitch_bot/cogs/play.py
@@ -1,4 +1,6 @@
 import random
+
+from twitchio.dataclasses import Context
 from twitch_bot.cogs.plugin import Plugin
 from twitchio.ext import commands
 
@@ -8,7 +10,7 @@ class PlayCog(Plugin):
         self.logger.warning(f"{self.__class__.__name__} is plugged in!")
 
     @commands.command(name="dice", aliases=['dados', 'dado'])
-    async def play_dice(self, ctx):
+    async def play_dice(self, ctx: Context) -> None:
         number = random.randint(0, 6)
         msg = self.bot.messages.commands['dice']
 
